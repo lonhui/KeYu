66c6f46cc42bc00cdca3a14aff22fe80c0c3ab6f
long <longhui@yogrt.co>
2020-02-03 11:46:57 +0800

    提交

---
+++ assets/js/data.js
@@ -0,0 +1,118 @@
+const Index = {
+    //轮播图地址
+    BANNER : [
+       "assets/images/banner/banner-2.jpg",
+       "assets/images/banner/banner-3.jpg"
+   ],
+   //文案
+   CULTURE:{
+       title:"深圳可昱床垫 始于出口品质",
+       context:"可昱床垫是深圳可昱睡眠科技有限有限公司旗下品牌。深圳可昱睡眠科技有限公司一直以来以出口高端床垫为主，年出口量达十几万张，从2013年开始由外贸进军国内高端市场，现已为数十万用户提供高品质的睡眠。可昱致力于把优质的床垫产品带给广大用户。为了保证高品质的质量，可昱先后在深圳宝安区工厂专门设立睡眠实验室，集研发制造为一体的床垫制造工厂。秉承质量为核心，可昱床垫严格对原材料采购和生产工艺把关，制造符合并高于行业标准的一流产品。工厂机器均采用和进口品牌工厂相同机械，均符合欧美标准的原材料并经过深圳专业检测部门检测。可昱制造的每件床垫产品，承载着品牌声誉和技术精髓，一如既往地为用户提供舒适、健康的睡眠体验"
+   },
+   HOT_PRODUCTS:[
+       {
+        id:1,
+        url:"assets/images/mattress/mattress_10.jpg",
+        title:"xxx",
+        type:"床垫"
+       },
+       {
+        id:2,
+        url:"assets/images/mattress/mattress_6.jpg",
+        title:"xxx",
+        type:"床垫"
+       },
+       {
+        id:3,
+        url:"assets/images/mattress/mattress_3.jpg",
+        title:"xxx",
+        type:"床垫"
+       },
+       {
+        id:4,
+        url:"assets/images/mattress/mattress_7.jpg",
+        title:"xxx",
+        type:"床垫"
+       }
+   ],
+   FOOTER_PRODUCT:[
+       "assets/images/index_footer/index_fotter_1.jpg",
+       "assets/images/index_footer/index_fotter_2.jpg",
+       "assets/images/index_footer/index_fotter_3.jpg",
+       "assets/images/index_footer/index_fotter_5.jpg",
+       "assets/images/index_footer/index_fotter_6.jpg",
+       "assets/images/index_footer/index_fotter_7.jpg"
+   ]
+}
+
+// window.onload = function(){
+    /*****/
+    // let banner = "";
+    // if(Index.BANNER.length > 0){
+    //     for(let i = 0; i < Index.BANNER.length; i++){
+    //         banner = banner + 
+    //         "<div class=\"banner\">"+
+    //             "<div class=\"banner-d\">"+
+    //                 "<img src=\"" + Index.BANNER[i] + "\" alt=\"\" >"+
+    //                 "<div class=\"bg-image-overlay\"></div>"+
+    //                 "<div class=\"nk-gap-3 mt-10\"></div>"+
+    //             "</div>"
+    //         +"</div>";
+    //     }
+    // }
+    // this.document.getElementById("index_banner").innerHTML = banner;
+    /*****/
+    // this.document.getElementById("about").getElementsByTagName("h2").innerHTML += Index.CULTURE.title;
+    /*****/
+    
+    /** */
+    // let = footerProducts = "";
+    // if(Index.FOOTER_PRODUCT.length > 0){
+    //     for(let i = 0; i < Index.FOOTER_PRODUCT.length; i++){
+    //         footerProducts = footerProducts + 
+    //        "<div>"+
+    //             "<div>"+
+    //                 "<div class=\"nk-box-1\">"+
+    //                     "<img src=\""+ Index.FOOTER_PRODUCT[i] +"\" alt=\"\" class=\"nk-img-fit\">"+
+    //                 "</div>"+
+    //             "</div>"+
+    //         "</div>"
+    //     }
+    // }
+    // this.document.getElementById("footer_products").innerHTML = footerProducts;
+// }
+const About = {
+    KELI:[
+        {
+            id:1,
+            label:"文化",
+            title:"企业文化",
+            context:"可昱床垫是深圳可昱睡眠科技有限有限公司旗下品牌。深圳可昱睡眠科技有限公司一直以来以出口高端床垫为主，年出口量达十几万张，从2013年开始由外贸进军国内高端市场，现已为数十万用户提供高品质的睡眠。可昱致力于把优质的床垫产品带给广大用户。为了保证高品质的质量，可昱先后在深圳宝安区工厂专门设立睡眠实验室，集研发制造为一体的床垫制造工厂。秉承质量为核心，可昱床垫严格对原材料采购和生产工艺把关，制造符合并高于行业标准的一流产品。工厂机器均采用和进口品牌工厂相同机械，均符合欧美标准的原材料并经过深圳专业检测部门检测。可昱制造的每件床垫产品，承载着品牌声誉和技术精髓，一如既往地为用户提供舒适、健康的睡眠体验"
+        },
+        {
+             id:2,
+             label:"文化",
+             title:"品牌文化",
+             context:"不仅对家庭，可昱还经受住了五星级酒店苛刻的酒店标准考验。多年来，可昱为超过50家五星级酒店提供高品质睡眠服务。在诸多五星级酒店内，你都可以看到可昱床垫的身影。 可昱持续的创新技术，可昱相信只有持续不断的技术创新才能让产品更好的服务用户。可昱和国内外知名骨科矫形专家合作，共同进行对人体身形特征等方面的研究，进而吸收运用于产品研发。在这基础上，可昱床垫在对提供高品质睡眠方面的技术得到进一步的提升和创新，并为可昱的产品奠定的优势地。 深圳可昱睡眠研究中心针对中国年轻人日益加剧的工作、生活压力大的现状，深圳可昱睡眠针对中国年轻人专业打造优化睡眠的床垫，保证对人体脊椎的保护外，更增强了床垫对睡眠的舒适度，在极具性价比的前提下，能够有效改善有失眠现象的年轻人的睡眠质量，让年轻人有能力和精力享受正确好的舒适睡眠。 保持技术创新的秘诀还在于可昱精益求精的探索精神，为了不断的创新和研发，可昱投入庞大的资源及财力，研究人体睡眠和开发改良产品，创建了大规模和先进的研发中心和产品测试实验室，一根线、一个角度，甚至于一丝承重力的改变都会被悉心研究。不断的创新和研发正是可昱品质保证的秘诀，也是可昱不断提升自身价值的源泉。可昱将专家多年科研成就与自身的睡眠系统设计有效结合，为每个“身体”提供正确承托。针对不同个体的正确承托让您全身肌肉得以松弛，获得真正充分的休息。一觉醒来，自然感觉精神饱满、活力充沛。"
+         }
+    ]
+ }
+ const Blog = {
+    QuestionAndAnswer:[
+        {
+            id:1,
+            question:"床垫使用过一段时间后，出现了人体压痕怎么办?",
+            answer:"一张舒适的床垫皆经过特别设计，所使用的材料都在一定程度上会有形变的范围，在经过一段时间的使用之后，会因为材料的压力感应以及空气被挤压掉等综合原因，产生人体压痕。此时请别担心，这是品质较好的床垫所出现的正常现象，这便意味着床垫垫层已经自然的顺应消费者的身形，绝非弹簧凹陷。人体压痕若在床垫总高度的10%的范围内是正常的，这说明产品符合人体工程学设计，并且在一定程度上提高了床软垫的舒适度。如要平衡这些凹痕，可定期翻动床垫。"
+        },
+        {
+             id:2,
+             question:"床垫弄湿或弄脏了怎么办?",
+             answer:"正常情况下，我们会在床垫上铺上床罩，所以弄脏床垫一般不会发生。但是弄湿床垫就有可能了，不管是果汁、饮料或小朋友尿床等流质的东西，水分会渗透床罩流到床垫上，如果真的发生了，请赶快用一块干抹布，用压摁的方式迅速吸干水分，同时将床垫竖立一会儿，让它晾干避免水分一直渗透到床底。市面上有销售床垫专用的保洁垫，如果能在床垫上先铺保洁垫，再铺床罩就更好了。"
+        },
+        {
+            id:3,
+            question:"为什么床垫会下陷?",
+            answer:"造成床垫下陷的因素很多，这包含了使用承托力欠佳的床架、不正确的排骨架床架、损坏变形的床架，或是已受损的弹簧床架。请使用正确的床架，好的床架可增加上垫的弹性及舒适度，并且减少弹簧下陷的机会。"
+        }
+    ]
+ }
